ae94090e28254bec298175e8b6cfb34d27bd608c
Chris White <cwhite@defaqto.com>
2016-11-14 13:59:09 +0000

    First Commit of DI

---
+++ Application.cs
@@ -0,0 +1,28 @@
+using Microsoft.Extensions.Logging;
+
+namespace Logging {
+    public class Application {
+
+        ILoggerFactory _logging = null;
+
+        ILogger Log { get {
+            return _logging?.CreateLogger<Application>();
+        }}
+
+        public Application (ILoggerFactory loggerFactory)
+        {    
+            _logging = loggerFactory;
+        }
+
+        public void Run() {
+            Log.LogInformation("Hello World");
+
+            Log.LogWarning("Oh no the world might blow");
+
+            Log.LogError("Oh my god the world is gonna blow!");
+
+            
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+
+namespace Logging
+{
+    public class Program
+    {
+        static IServiceProvider serviceProvider = null;
+
+        public static void Main(string[] args)
+        {
+            IServiceCollection serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            serviceProvider = serviceCollection.BuildServiceProvider();
+            Configure();
+            
+            Application app = serviceProvider.GetRequiredService(typeof(Application)) as Application;
+            if (app != null) {
+                app.Run();
+            }
+            else {
+                throw new Exception("No application instance. Check your dependencies.");
+            }
+        }
+
+        private static void Configure() {
+            var loggingFactory = serviceProvider.GetRequiredService(typeof(ILoggerFactory)) as ILoggerFactory;
+            loggingFactory.AddConsole();
+        }
+
+        private static void ConfigureServices(IServiceCollection serviceCollection) {
+            serviceCollection.AddLogging();
+            serviceCollection.AddSingleton<Application>();
+        }
+
+    }
+}
